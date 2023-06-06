5566aac9768d5747e4d25f6e3041600d0f74d5ea
JaydeepDas2000 <jaydeep.das@outlook.in>
2023-06-06 22:26:20 +0530

    Login issue resolve

--- src/main/java/com/springrest/springrest/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springrest.springrest.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.springrest.springrest.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
